58a1909db6d6aac26d1de89fdc1ac9f716aa31a3
pennebakers <fusecreated@gmail.com>
2022-08-09 10:25:02 -0400

    adding base files to master branch

---
+++ src/main/java/com/foodMenu/Menu/MenuApplication.java
@@ -0,0 +1,13 @@
+package com.foodMenu.Menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuApplication.class, args);
+	}
+
+}
