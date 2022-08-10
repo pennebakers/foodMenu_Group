1e012536a6f4442ade4a2ef6d79e9baa73d1fb7b
DominickLidwin <dlidwin@xpanxion.com>
2022-08-10 11:41:33 -0400

    foodlist

---
+++ Frontend/src/app/food-details/food-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FoodDetailsComponent } from './food-details.component';
+
+describe('FoodDetailsComponent', () => {
+  let component: FoodDetailsComponent;
+  let fixture: ComponentFixture<FoodDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FoodDetailsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FoodDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
