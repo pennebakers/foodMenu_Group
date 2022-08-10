835b59616bbe7052d7f839397782001d2b29985a
Seth Pennebaker <fusecreated@gmail.com>
2022-08-10 10:59:53 -0400

    adding files

---
+++ Frontend/src/app/create-drink/create-drink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-drink',
+  templateUrl: './create-drink.component.html',
+  styleUrls: ['./create-drink.component.css']
+})
+export class CreateDrinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Frontend/src/app/drink-list/drink-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drink-list',
+  templateUrl: './drink-list.component.html',
+  styleUrls: ['./drink-list.component.css']
+})
+export class DrinkListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
