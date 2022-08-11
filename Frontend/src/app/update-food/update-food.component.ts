f59bdc10278fc3c8f0a6ce22a93800eccd5c4a60
Seth Pennebaker <fusecreated@gmail.com>
2022-08-11 16:30:26 -0400

    adding files

--- Frontend/src/app/update-food/update-food.component.ts
+++ Frontend/src/app/update-food/update-food.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Food } from '../food';
+import { FoodService } from '../food-service';
 
 @Component({
   selector: 'app-update-food',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateFoodComponent implements OnInit {
 
-  constructor() { }
+ 
+  id!: number;
+  food: Food = new Food();
+  constructor(private foodService: FoodService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.foodService.getFoodById(this.id).subscribe(data => {
+      this.food = data;
+    }, error => console.log(error));
+
+  }
+
+  onSubmit(){
+    this.foodService.updateFood(this.id, this.food).subscribe(data => {
+      console.log(data);
+      this.goToFoodList();
+    }, error => console.log(error));
+  }
+
+  goToFoodList(){
+    this.router.navigate(['/employees']);
   }
 
 }
