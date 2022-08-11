8bdc73b11fd3e8abcd9c5c53bf52d9659a3baf4b
Seth Pennebaker <fusecreated@gmail.com>
2022-08-11 09:51:28 -0400

    adding correct files

--- Backend/FoodMenu/FoodMenu/src/main/java/com/example/FoodMenu/FoodMenuApplication.java
+++ Backend/FoodMenu/FoodMenu/src/main/java/com/example/FoodMenu/FoodMenuApplication.java
@@ -3,7 +3,7 @@ package com.example.FoodMenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.FoodMenu.repository.FoodMenuRepository")
+@SpringBootApplication
 public class FoodMenuApplication {
 
 	public static void main(String[] args) {
